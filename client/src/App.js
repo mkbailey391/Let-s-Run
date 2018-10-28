c51874afc91774cc6d5b31f33c8a0a96d829b682
Megan Bailey <mkbailey391@gmail.com>
2018-10-28 12:28:15 -0700

    creates login form

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,27 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Layout from './components/common/Layout/Layout';
 import Navbar from './components/common/Navbar/Navbar';
+import Login from './components/Login/Login';
+import httpClient from './utilities/httpClient';
 
 
 class App extends Component {
+  state = { currentUser: httpClient.getCurrentUser() };
+
+  onAuthSuccess = () => {
+    this.setState({ currentUser:httpClient.getCurrentUser() });
+  }
+
   render() {
+    let { currentUser } = this.state;
+    let { onAuthSuccess } = this;
     return (
       <Layout>
         <Switch>
           <Route exact path = "/" component={Home}/>
+          <Route path ="/Login" render={(props) =>{
+            return <Login {...props} onLoginSuccess={onAuthSuccess}/>
+          }}/>
         </Switch>
       </Layout>
     )
