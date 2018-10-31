d1aa5d3d4184eb2853249ce1130ca1fa65387929
Megan Bailey <mkbailey391@gmail.com>
2018-10-31 16:57:42 -0700

    updates home page.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import Header from '../common/Header/Header';
-import Card from 'react-bootstrap/lib/Card';
+import { Link } from 'react-router-dom';
+import Card from '../common/Card/Card';
 
-export default ({ groups }) => {
 
      /*
         1. use the map method on props.groups
         2. for each group we are on return the card with the particular group
      */
-     return (
+
+class Home extends Component {
+    state = {
+        groups: [],
+        message: ""
+    }
+async componentDidMount() {
+
+    let response = await axios.get('/api/groups');
+    let { groups } = response.data;
+    if (groups.length > 0) {
+        this.setState({ groups });
+    } else {
+        this.setState({ message: "No groups to display"})
+    }
+}
+renderGroups = () => {
+    return this.state.groups.map(g => {
+        return (
+            <Card group={g}/>
+        )
+    })
+}
+render(){
+    return(
         <div>
-            
+            {this.renderGroups}
         </div>
     )
 }
+}
+
+export default Home;
---
+++ client/src/components/common/Card/Card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default ({ group }) => {
+    return (
+        <div key={group._id} className="card is-light column is-one-quarter">
+            <div className="card-image">
+                <img src={group.image}/>
+            </div>
+            <div className="card-content">
+                <div className="media-content" style={{ textAlign: "center" }}>
+                    <div className="columns">
+                        <p className="title is-4" style={{ textAlign: "center" }}>{group.name}</p>
+                    </div>
+                    <div className="columns is-multiline">
+                        <div className="column">
+                            <Link className="button is-primary" style={{ width: "100%" }} to={`/groups/${group._id}`}>Show</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
