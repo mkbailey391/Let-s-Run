1f56c2eee4886e2d93640ad029acad7301841da4
Megan Bailey <mkbailey391@gmail.com>
2018-10-31 16:11:34 -0700

    reverted back to milligram.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -10,20 +10,7 @@ export default ({ groups }) => {
      */
      return (
         <div>
-            {groups.map(({name, image}) => {
-                return (
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={image} />
-                        <Card.Body>
-                            <Card.Title>{name}</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                )})
-            }
+            
         </div>
     )
 }
